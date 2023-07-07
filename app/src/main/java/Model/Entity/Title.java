135a66abb91cd14680540f691845508d87af6055
Fei Yin <yinfei_duq@petalmail.com>
2023-07-07 20:15:55 +0800

    添加几个前端组件

--- app/src/main/java/Model/Entity/Title.java
+++ app/src/main/java/Model/Entity/Title.java
@@ -14,10 +14,23 @@ public class Title {
     }
 
     // 获取称号名称
-    public String getName(Player player) {
-
-        return name;
+    public String Id_Name(int id) {
+        for (Title title : TITLES) {
+            if (title.getId() == id) {
+                return title.getName();
+            }
+        }
+        return null; // 如果没有找到对应id的Title，则返回null或者你认为合适的默认值
     }
+
+    private String getName() {
+        return this.name;
+    }
+
+    private int getId() {
+        return this.Id;
+    }
+
 
     // 获取称号描述
     public String getDescription() {
@@ -26,12 +39,13 @@ public class Title {
 
     // 称号列表
     public static final Title[] TITLES = {
+            new Title(0,"平平无奇","未获得任何成就"),
             new Title(1,"初尝胜果", "赢得1局游戏"),
-            new Title(2,"小有所成", "赢得10局游戏"),
-            new Title(3,"游戏达人", "赢得100局游戏"),
-            new Title(4,"顺风顺水", "连胜5局"),
-            new Title(5,"常胜将军", "连胜10局"),
-            new Title(6,"打酱油的", "在一局游戏中，一张牌也没有出"),
-            new Title(7,"行云流水", "同花顺五张")
+            new Title(10,"小有所成", "赢得10局游戏"),
+            new Title(100,"游戏达人", "赢得100局游戏"),
+            new Title(200,"顺风顺水", "连胜5局"),
+            new Title(300,"常胜将军", "连胜10局"),
+            new Title(400,"打酱油的", "在一局游戏中，一张牌也没有出"),
+            new Title(800,"行云流水", "同花顺五张")
     };
 }
